1f067ee88c309708e9ec22f34b635f6f13bb7b55
marvinbra <marvinbopp37@gmail.com>
2026-03-22 17:10:33 -0300

    getDataByParams.js code

--- controllers/getDataByPathParams.js
+++ controllers/getDataByPathParams.js
@@ -1,41 +1,18 @@
 import { startups } from '../data/data.js'
 
-export const getAllData = (req, res) => {
+export const getDataByPathParams = (req, res) => {
 
-  let filteredData = startups
+  const { field, term } = req.params
 
-  const { industry, country, continent, is_seeking_funding, has_mvp } = req.query
+  const allowedFields = ['country', 'continent', 'industry']
 
-  if (industry) {
-    filteredData = filteredData.filter(startup =>
-      startup.industry.toLowerCase() === industry.toLowerCase()
-    )
+  if (!allowedFields.includes(field)) {
+    return res.status(400).json({ message: "Search field not allowed. Please use only country, continent, industry" })
   }
 
-  if (country) {
-    filteredData = filteredData.filter(startup =>
-      startup.country.toLowerCase() === country.toLowerCase()
-    )
-  }
-
-  if (continent) {
-    filteredData = filteredData.filter(startup =>
-      startup.continent.toLowerCase() === continent.toLowerCase()
-    )
-  }
-
-  if (is_seeking_funding) {
-    filteredData = filteredData.filter(startup =>
-      startup.is_seeking_funding === JSON.parse(is_seeking_funding.toLowerCase())
-    )
-  }
-
-  if (has_mvp) {
-    filteredData = filteredData.filter(startup =>
-      startup.has_mvp === JSON.parse(has_mvp.toLowerCase())
-    )
-  }
+  const filteredData = startups.filter(
+    startup => startup[field].toLowerCase() === term.toLowerCase()
+  )
 
   res.json(filteredData)
-
 }
